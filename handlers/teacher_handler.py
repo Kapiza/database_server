06d916ba2082ab130f3d5f550fbad46e89ef91c4
user <user@P301-1.uitnet.int.sibsau.ru>
2025-04-24 17:32:33 +0700

    complete teacher handler prototype

--- handlers/teacher_handler.py
+++ handlers/teacher_handler.py
@@ -7,20 +7,23 @@ def teacher_handler(handler):
         sql = """
             SELECT * FROM teacher 
         """
-        cursor.execute(sql, (id, name))
-
+        cursor.execute(sql)
         rows = cursor.fetchall()
 
-        # handler.db.commit()
+
         # 
+        columns = [desc[0] for desc in cursor.description]
+        # Список словарей
+        data = [ dict(zip(columns, row)) for row in rows ]
 
         # Response
         handler.send_response(200)
-        handler.send_header("Content-type", "text/html")
+        handler.send_header("Content-type", "application/json")
         handler.end_headers()
-        handler.wfile.write(f"HI".encode("utf-8"))
+        handler.wfile.write(json.dumps(data, default=str).encode("utf-8"))
+        
     elif handler.command == "POST":
-        # GET DATA
+        # GET request data
         content_length = int(handler.headers.get('Content-Length', 0))
         body = handler.rfile.read(content_length)
         data = json.loads(body)
@@ -47,6 +50,48 @@ def teacher_handler(handler):
         handler.wfile.write(f"HI".encode("utf-8"))
         # 
     elif handler.command == "PUT":
-        ...
+
+        # GET request data
+        content_length = int(handler.headers.get('Content-Length', 0))
+        body = handler.rfile.read(content_length)
+        data = json.loads(body)
+
+        # 
+        name = data["name"]
+
+        # SQL
+        cursor = handler.db.cursor()
+        sql = """
+            UPDATE teacher 
+            SET name = %s
+            WHERE id = %s;
+        """
+        # cursor.execute(sql, (handler.path_params["id"]))
+        cursor.execute(sql, (name, handler.path_params["id"],))
+
+
+        # Response
+        handler.send_response(200)
+        handler.send_header("Content-type", "text/html")
+        handler.end_headers()
+        handler.wfile.write(f"CHANGED".encode("utf-8"))
+        # 
+
     elif handler.command == "DELETE":
-        ...
+
+        # SQL
+        cursor = handler.db.cursor()
+        sql = """
+            DELETE FROM teacher 
+            WHERE id=%s;
+        """
+        # cursor.execute(sql, (handler.path_params["id"]))
+        cursor.execute(sql, (handler.path_params["id"],))
+
+
+        # Response
+        handler.send_response(200)
+        handler.send_header("Content-type", "text/html")
+        handler.end_headers()
+        handler.wfile.write(f"DELETED".encode("utf-8"))
+        # 
